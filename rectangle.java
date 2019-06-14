6ad9b42ff54e047c71207dd8701b1b7a912fc2ad
NguyenThanhNHan01121994 <50906464+NguyenThanhNHan01121994@users.noreply.github.com>
2019-06-14 16:33:34 +0700

    Add files via upload
    tinh dien tich hinh chu nhật

--- rectangle.java
+++ rectangle.java
@@ -17,19 +17,22 @@ public class rectangle {
 			return w*h;
 		}
 
-	}
-	class main
-	{
-		public static void main ()
-		{
+	
+
+	
+		public static void main(String[] args) {
+		
 			rectangle r;
 			r = new rectangle();
 			//r.w = 10.0f;
 			//r.h= 20.0f;
 			r.setEdges(10.0f, 20.0f);
 			float dt = r.calSquare();
-			
+			System.out.println("dien tich hinh cn la: "+ dt);
 		}
 	}
-
+	
+	
+	
+	
 
